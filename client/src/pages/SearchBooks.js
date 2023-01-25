2f39d7add4fd46cadc7f5813b6870cde865489ad
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-25 16:42:20 -0700

    import and implement mutations in searchbooksjs

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -27,14 +28,14 @@ const SearchBooks = () => {
     if (!searchInput) {
       return false;
     }
-
+    // make request to Google Books API
     try {
       const response = await searchGoogleBooks(searchInput);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
-
+      // destructure response json and use mutation to save book to user's account
       const { items } = await response.json();
 
       const bookData = items.map((book) => ({
@@ -51,6 +52,8 @@ const SearchBooks = () => {
       console.error(err);
     }
   };
+  // create mutation for saving a book to user's account
+  const [saveBook, { error: bookError }] = useMutation(SAVE_BOOK);
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
@@ -63,11 +66,13 @@ const SearchBooks = () => {
     if (!token) {
       return false;
     }
-
+    // use mutation to save book to user's account
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({ 
+        variables: { input: { ...bookToSave } }
+      });
 
-      if (!response.ok) {
+      if (bookError) {
         throw new Error('something went wrong!');
       }
 
