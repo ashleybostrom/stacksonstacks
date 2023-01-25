c3b2c8ec6d1d938c20b0ea6d1f0f09c3551194ae
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-25 16:03:30 -0700

    added schemas folder w resolvers and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+// This file defines the typeDefs for the GraphQL schema
+
+const typeDefs = `#graphql
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String!
+        bookCount: Int
+        savedBooks: [Book!]
+    }
+
+    type Book {
+        _id: ID
+        authors: [String]
+        title: String!
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+    }
+
+    input BookInput {
+        _id: ID
+        authors: [String]
+        title: String!
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        user(email: String!): User
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(input: BookInput!): User
+        deleteBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
