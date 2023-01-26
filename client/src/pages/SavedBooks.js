4a292dd4f190f9a4dff2f0331f6de6ed2051dce0
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-25 22:45:38 -0700

    import react in savedbooksjs

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
