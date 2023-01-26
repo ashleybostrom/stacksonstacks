6c0cfe7cc3e96cbac01f102925d8b81a76390931
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-25 23:25:18 -0700

    changed mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/bookstacks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookstacks');
 
 module.exports = mongoose.connection;
